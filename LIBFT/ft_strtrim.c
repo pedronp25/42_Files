22ada7861a327d0054f6079959ad1a522f2732fd
Pedro Miguel Silva Pereira <pedromig@c1r4s5.42porto.com>
2025-04-14 19:23:03 +0100

    Update before ft_split

--- LIBFT/ft_strtrim.c
+++ LIBFT/ft_strtrim.c
@@ -6,13 +6,45 @@
 /*   By: pedromig <pedromig@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/14 01:03:45 by pedromig          #+#    #+#             */
-/*   Updated: 2025/04/14 01:06:59 by pedromig         ###   ########.fr       */
+/*   Updated: 2025/04/14 19:22:23 by pedromig         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+int	is_set(char const c, char const *set);
+
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	
+	int	beg;
+	int	end;
+
+	beg = 0;
+	end = ft_strlen(s1);
+	while (is_set(s1[beg], set))
+		beg++;
+	while (is_set(s1[end - 1], set))
+		end--;
+	return (ft_substr(s1, beg, (end - beg)));
 }
+
+int	is_set(char const c, char const *set)
+{
+	int	i;
+
+	i = 0;
+	while (set[i])
+	{
+		if (c == set[i])
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+/*
+int	main(void)
+{
+	printf("Returned String: %s\n", ft_strtrim("cabbcameowaababc", "abc"));
+}
+*/
--- LIBFT/libft.h
+++ LIBFT/libft.h
@@ -6,7 +6,7 @@
 /*   By: pedromig <pedromig@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/07 03:39:37 by pedromig          #+#    #+#             */
-/*   Updated: 2025/04/14 01:13:36 by pedromig         ###   ########.fr       */
+/*   Updated: 2025/04/14 18:30:01 by pedromig         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,6 +48,7 @@ char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 char	*ft_strnstr(const char *big, const char *little, size_t len);
 char	*ft_strrchr(const char *s, int c);
+char	*ft_strtrim(char const *s1, char const *set);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 int		ft_tolower(int c);
 int		ft_toupper(int c);
