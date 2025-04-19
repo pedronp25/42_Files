efc6bdc64720ce6d5d3951d1e799b04601169ba0
Pedro Miguel Silva Pereira <pedromig@c1r4s3.42porto.com>
2025-04-19 17:56:25 +0100

    Updates

--- GET_NEXT_LINE/get_next_line.c
+++ GET_NEXT_LINE/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: pedromig <pedromig@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/19 15:03:45 by pedromig          #+#    #+#             */
-/*   Updated: 2025/04/19 17:09:29 by pedromig         ###   ########.fr       */
+/*   Updated: 2025/04/19 17:53:46 by pedromig         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@
 	int	i;
 	size_t	bytes;
 	char	*buf;
+	char	*ret_str;
 
 	buf = malloc(BUFFER_SIZE);
 	if (!buf)
@@ -30,11 +31,72 @@
 		buf[bytes] = '\0';
 		while (buf[i] != '\n' && buf[i])
 			i++;
-		if (buf[i] == '\n' || (!buf[i] && buf[i] != buf[bytes])) //Check if string has '\n' or if it has reached the end of the file (if it has reached a null and it hasn't filled the buffer)
+		if (buf[i] == '\n' || (!buf[i] && i != bytes)) //Check if string has '\n' or if it has reached the end of the file (if it has reached a null and it hasn't filled the buffer)
+			break ;
+		else
+		{
+			ft_strjoin(ret_str, buf);
+			bytes = read(fd, buf, BUFFER_SIZE - 1);
+		}
 	}
+	return (ret_str);
 }
 
-void	*ft_realloc(void *ptr, size_t size)
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*final_str;
+	size_t	s1_len;
+	size_t	s2_len;
+	size_t	total_size;
+
+	s1_len = ft_strlen(s1) + 1;
+	s2_len = ft_strlen(s2) + 1;
+	total_size = s1_len + s2_len;
+	final_str = malloc(total_size - 1);
+	if (!final_str)
+		return (NULL);
+	ft_strlcpy(final_str, s1, s1_len);
+	ft_strlcat(final_str, s2, total_size);
+	return (final_str);
+}
+
+size_t	ft_strlcat(char *dst, const char *src, size_t size)
+{
+	size_t	dest_len;
+	size_t	src_len;
+	size_t	x;
+	size_t	y;
+
+	dest_len = ft_strlen(dst);
+	src_len = ft_strlen(src);
+	x = dest_len;
+	y = 0;
+	if (size == 0)
+		return (src_len);
+	while (src[y] && x < (size - 1))
+	{
+		dst[x] = src[y];
+		x++;
+		y++;
+	}
+	dst[x] = '\0';
+	if (size <= dest_len)
+		return (src_len + size);
+	else
+		return (src_len + dest_len);
+}
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	x;
+
+	x = 0;
+	while (s[x])
+		x++;
+	return (x);
+}
+
+void	*ft_realloc(void *ptr, size_t size) //Don't know if I'm going to use this
 {
 	void	*ptr2;
 
