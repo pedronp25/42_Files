5f5426b9d93f80ab5779c92285ddf6ff2183f014
Pedro Miguel Silva Pereira <pedromig@c1r5s4.42porto.com>
2025-06-15 22:15:35 +0100

    meow

--- 02_PIPEX/pipex.c
+++ 02_PIPEX/pipex.c
@@ -6,7 +6,7 @@
 /*   By: pedromig <pedromig@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/09 18:00:41 by pedromig          #+#    #+#             */
-/*   Updated: 2025/06/15 22:05:02 by pedromig         ###   ########.fr       */
+/*   Updated: 2025/06/15 22:15:23 by pedromig         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,8 @@ int	main(int argc, char *argv[], char *envp[])
 
 	// Check argc
 
-	pp = ft_calloc(1, sizeof(t_pipex));
-	// Check calloc
+	pipex_initializer(argv); // NOT DONE
+		//pp = ft_calloc(1, sizeof(t_pipex)); --> TO ADD INSIDE THE FUNCTION
 	
 	pp->fd1 = open(argv[1], O_RDONLY);
 	// Check open
