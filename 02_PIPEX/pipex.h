3ae32cc03e8fbf10a5ab571f5fa9b45821d70daa
Pedro Miguel Silva Pereira <pedromig@c1r4s5.42porto.com>
2025-06-18 22:16:47 +0100

    1st Test Version

---
+++ 02_PIPEX/pipex.h
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pedromig <pedromig@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/18 22:12:20 by pedromig          #+#    #+#             */
+/*   Updated: 2025/06/18 22:16:24 by pedromig         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+
+# define PIPEX_H
+
+# include "libft/libft.h"
+# include <fcntl.h>
+
+typedef struct	s_pipex
+{
+	int		fd1;
+	int		fd2;
+	int		pipe_fd[2];
+	int		id1;
+	int		id2;
+	char	**cmd1_args;
+	char	**cmd2_args;
+	char	**envp;
+	char	*cmd1_path;
+	char	*cmd2_path;
+}	t_pipex;
+
+
+void	child1(t_pipex *pp, char *cmd1, char **envp);
+void	child2(t_pipex *pp, char *cmd2, char **envp);
+t_pipex	*pipex_initializer(int argc, char **argv);Add commentMore actions
+void	pipex_error(char *err_msg, t_pipex *pp);
+void	pipex_cleanup(t_pipex *pp);
+void	pipex_free_arr(char **str);
+
+#endif
