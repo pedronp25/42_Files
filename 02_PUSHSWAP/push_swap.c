f5c6fe82bb324c99ed9a81c41c8f349d83bab3b8
pedronp25 <pedromsp500@gmail.com>
2025-07-07 02:24:37 +0100

    meow

--- 02_PUSHSWAP/push_swap.c
+++ 02_PUSHSWAP/push_swap.c
@@ -6,15 +6,42 @@
 /*   By: pedromig <pedromig@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/05 20:35:43 by pedromig          #+#    #+#             */
-/*   Updated: 2025/07/07 01:10:50 by pedromig         ###   ########.fr       */
+/*   Updated: 2025/07/07 02:24:13 by pedromig         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+#include "libft/libft.h"
 
 int	main(int argc, char *argv[])
 {
-	if (argc != 2)
-		ft_printf("Usage: ./push_swap \"list of numbers to sort\"\n");
+	char	**matrix;
 
+	matrix = ps_args_check(argc, argv);
+	// Need to validate matrix (check ChatGPT)
+}
+
+char	**ps_args_check(int argc, char **argv)
+{
+	char	**matrix;
+	int		x;
+
+	if (argc == 1)
+		exit (1);
+	if (argc == 2)
+	{
+		matrix = ft_split(argv[1], ' ');
+		if (!matrix)
+			exit (1);
+	}
+	matrix = ft_calloc(argc - 1, sizeof(char));
+	if (!matrix)
+		exit (1);
+	x = 1;
+	while (x < argc)
+	{
+		matrix[x - 1] = argv[x];
+		x++;
+	}
+	return (matrix);
 }
--- 02_PUSHSWAP/push_swap.h
+++ 02_PUSHSWAP/push_swap.h
@@ -6,14 +6,15 @@
 /*   By: pedromig <pedromig@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/05 20:36:29 by pedromig          #+#    #+#             */
-/*   Updated: 2025/07/05 20:37:45 by pedromig         ###   ########.fr       */
+/*   Updated: 2025/07/07 02:22:57 by pedromig         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
-#endif
 
 # include "libft/libft.h"
+
+char	**ps_args_check(int argc, char **argv);
 
 #endif
