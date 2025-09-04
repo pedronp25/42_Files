11f18cf71a091efd6ca8fa35b6edfda5dd7a5494
pedronp25 <pedromsp500@gmail.com>
2025-09-04 20:56:17 +0100

    Updates

---
+++ 03_PHILOSOPHERS/philosophers.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pedromig <pedromig@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/27 22:39:46 by pedromig          #+#    #+#             */
+/*   Updated: 2025/08/28 03:57:31 by pedromig         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philosophers.h"
+
+int	main(int argc, char **argv)
+{
+	
+}
+
+void	validate_arguments(int argc, char **argv)
+{
+	int	extra_arg;
+
+	if (argc == 5)
+		// Set 'extra_arg' to '0' (to use for init function)
+	else if (argc == 6)
+		// Set 'extra_arg' to '1' (to use for init function)
+	else
+		// Invalid arguments
+	
+}
