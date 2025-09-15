5a8c86d1a5cf5081d9b4bf2928fa6f14626e63dd
Pedro Miguel Silva Pereira <pedromig@c2r5s5.42porto.com>
2025-09-15 02:10:32 +0100

    Some updates in Philosophers

--- 03_PHILOSOPHERS/philosophers.c
+++ 03_PHILOSOPHERS/philosophers.c
@@ -6,7 +6,7 @@
 /*   By: pedromig <pedromig@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/27 22:39:46 by pedromig          #+#    #+#             */
-/*   Updated: 2025/09/14 18:54:49 by pedromig         ###   ########.fr       */
+/*   Updated: 2025/09/15 02:10:13 by pedromig         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,95 @@
 
 int	main(int argc, char **argv)
 {
-	
+	t_data 	*data;
+	t_philo	*philos;
+
+	data = ph_init_data(argc, argv);
+	if (!data)
+		return (1); // Couldn't initiate data; Cleanup left;
+	philos = ph_init_philos(); // TO DO
 }
 
-void	validate_arguments(int argc, char **argv)
+t_data	*ph_init_data(int argc, char **argv)
 {
-	int	extra_arg;
+	t_data	*data;
+	int		x;
 
-	if (argc == 5)
-		// Set 'extra_arg' to '0' (to use for init function)
-	else if (argc == 6)
-		// Set 'extra_arg' to '1' (to use for init function)
+	x = 0;
+	data = malloc(sizeof(*data));
+	if (!data)
+		return (NULL);
+	if (ph_validate_arguments(argc, argv, data))
+		return (NULL);
+	data->start_time = 0;
+	data->forks = malloc(sizeof(pthread_mutex_t) * data->n_philos);
+	if (!data->forks)
+		return (NULL);
+	while (x < data->n_philos)
+	{
+		pthread_mutex_init(&data->forks[x], NULL);
+		x++;
+	}
+	data->is_dead = 0;
+	pthread_mutex_init(&data->print_mutex, NULL);
+	pthread_mutex_init(&data->death_mutex, NULL);
+	return (data);
+}
+
+int	ph_validate_arguments(int argc, char **argv, t_data *data)
+{
+	if (argc != 5 && argc != 6)
+		return (-1); // Error! Not enough or too many arguments
+	if (!ph_is_digit(argv[1]) || !ph_is_digit(argv[2]) ||
+			!ph_is_digit(argv[3]) || !ph_is_digit(argv[4]))
+		return (-1); // Error! Invalid arguments
+	data->n_philos = ph_atol(argv[1]);
+	data->time_die = ph_atol(argv[2]);
+	data->time_eat = ph_atol(argv[3]);
+	data->time_sleep = ph_atol(argv[4]);
+	if (argc == 6)
+	{
+		if (!ph_is_digit(argv[5]) || ph_atol(argv[5]) == 0)
+			return (-1); // Error! Invalid 5th argument
+		data->n_meals = ph_atol(argv[5]);
+	}
 	else
-		// Invalid arguments
-	
+		data->n_meals = -1; // Special value = Infinite meals
+	if (data->n_philos <= 0 || data->time_die <= 0 ||
+			data->time_eat <= 0 || data->time_sleep <= 0)
+		return (-1); // Error! Arguments must be greater than 0
+	return (0); // Success!
+}
+
+int	ph_is_digit(char *num)
+{
+	int	x;
+
+	x = 0;
+	while (num[x])
+	{
+		if (num[x] < '0' || num[x] > '9')
+			return (-1);
+		x++;
+	}
+	return (1);
+}
+
+long	ph_atol(char *str)
+{
+	long	result;
+	int		digit;
+	int		x;
+
+	result = 0;
+	x = 0;
+	while (str[x])
+	{
+		digit = str[x] - '0';
+		if (result > (9223372036854775807 - digit) / 10)
+			return (-1); // Error! Number bigger than LONG_MAX
+		result = result * 10 + (str[x] - '0');
+		x++;
+	}
+	return (result);
 }
--- 03_PHILOSOPHERS/philosophers.h
+++ 03_PHILOSOPHERS/philosophers.h
@@ -6,7 +6,7 @@
 /*   By: pedromig <pedromig@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/13 17:59:46 by pedromig          #+#    #+#             */
-/*   Updated: 2025/09/14 18:54:48 by pedromig         ###   ########.fr       */
+/*   Updated: 2025/09/15 01:22:45 by pedromig         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,19 +22,37 @@
 
 typedef struct s_data
 {
-	int		n_philos; // Number of philosophers
-	int		time_die; // Time to die (milisseconds)
-	int		time_eat; // Time to eat (milisseconds)
-	int		time_sleep; // Time to sleep (milisseconds)
-	int		n_meals; // Number of meals a philosopher must have (Optional)
+	long			n_philos; // Number of philosophers
+	long			time_die; // Time to die (milisseconds)
+	long			time_eat; // Time to eat (milisseconds)
+	long			time_sleep; // Time to sleep (milisseconds)
+	long			n_meals; // Number of meals a philosopher must have (Optional)
 	
-	int		is_dead; // Value to stop simulation when someone dies
-	long	start_time; // Start time in milisseconds
+	int				is_dead; // Value to stop simulation when someone dies
+	long			start_time; // Start time in milisseconds
+	
+	pthread_mutex_t	*forks;
+	pthread_mutex_t	print_mutex;
+	pthread_mutex_t	death_mutex;
+
 }	t_data;
 
 typedef struct s_philo
 {
-	// To do
+	int				id; // This philosopher's ID
+	int				meals_eaten; // Number of meals eaten for this philosopher
+	long			time_last_meal; // Timestamp of last meal eaten
+	
+	pthread_t		thread; // This philosopher's thread;
+	pthread_mutex_t	*left_fork;
+	pthread_mutex_t	*right_fork;
+
 }	t_philo;
+
+
+t_data	*ph_init_data(int argc, char **argv);
+int		ph_validate_arguments(int argc, char **argv, t_data *data);
+int		ph_is_digit(char *num);
+long	ph_atol(char *str);
 
 #endif
